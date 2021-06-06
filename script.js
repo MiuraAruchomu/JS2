6fcfab9c4d3c8b694afc5e094a966de8727e92ea
MiuraAruchomu <miura.aruchomu@gmail.com>
2021-06-06 19:03:50 +0300

    commit less3

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 const API_URL = "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses"
 
-class GoodsItem {
-    constructor(title, price, id) {
-        this.title = title
-        this.price = price
-        this.id = id
-    }
-
-    render() {
-        return `<div class="goods-item" data-title="${this.title}" data-price="${this.price}" data-id="${this.id}"><h3>${this.title}</h3><p>${this.price}</p><button class="add-to-cart-button" type="button"><i class="fas fa-shopping-basket"></i></button></div>`
-    }
-}
-
 class GoodsList {
     constructor() {
         this.goods = []
+        this.clickHandler =''
     }
 
     async fetchGoods() {
@@ -36,22 +25,17 @@ class GoodsList {
         document.querySelector(".goods-list").innerHTML = listHtml
     }
 
-    clickHandlers() {
-        const buttonsEl = document.querySelectorAll('.add-to-cart-button')
-        buttonsEl.forEach(button => {
+    clickHandlers(cartList) {
+        this.clickHandler = document.querySelectorAll('.add-to-cart-button')
+        this.clickHandler.forEach(button => {
             button.addEventListener('click', (event) => {
-                let listHTML = ''
-    
                 let good = event.target.closest('div')
                 let goodTitle = good.dataset.title
                 let goodPrice = good.dataset.price
-                let goodId = good.dataset.id
-    
+                let goodId = good.id
+
                 let cartItem = new CartItem(goodTitle, goodPrice, goodId)
-                listHTML += cartItem.render()
-                
-                let cartList = document.querySelector('.cart-list')
-                сartList.innerHTML += listHTML
+                cartList.addItemToCart(cartItem)
             })
         })
     }
@@ -65,6 +49,78 @@ class GoodsList {
     }
 }
 
+class GoodsItem {
+    constructor(title, price, id) {
+        this.title = title
+        this.price = price
+        this.id = id
+    }
+
+    render() {
+        return `<div class="goods-item" data-title="${this.title}" data-price="${this.price}" id="${this.id}"><h3>${this.title}</h3><p>${this.price}</p><button class="add-to-cart-button" type="button"><i class="fas fa-shopping-basket"></i></button></div>`
+    }
+}
+
+class Cart {
+    constructor() {
+        this.goods = []
+        this.clickHandler = ''
+    }
+
+    addItemToCart(cartItem) {
+        let flag = false
+        this.goods.forEach(item => {
+            if (item.id === cartItem.id) {
+                flag = true
+            }
+        })
+        if (flag) {
+            alert('Данный товар уже находится в корзине')
+        } else {
+            this.render(cartItem)
+        }
+    }
+
+    removeItemFromCart(cartItem, cartItemId) {
+        let flag = false
+        let good = ''
+        this.goods.forEach(item => {
+            while (true) {
+                if (item.id === cartItemId) {
+                    flag = true
+                    good = item
+                }
+                break
+            }
+        })
+        if (flag) {
+            this.goods = this.goods.filter(item => item !== good)
+            cartItem.remove()
+            this.clickHandlers()
+        }
+    }
+
+    clickHandlers() {
+        this.clickHandler = document.querySelectorAll('.remove-from-cart-button')
+        this.clickHandler.forEach(button => {
+            button.addEventListener('click', (event) => {
+                let good = event.target.closest('div')
+                let goodId = good.id
+
+                this.removeItemFromCart(good, goodId)
+            })
+        })
+    }
+
+    render(cartItem) {
+        this.goods.push(cartItem)
+        document.querySelector('.cart-list').innerHTML += cartItem.render()
+        this.clickHandlers()
+    }
+
+
+}
+
 class CartItem {
     constructor(title, price, id) {
         this.title = title
@@ -73,23 +129,17 @@ class CartItem {
     }
 
     render() {
-        return `<div class="cart-item" data-title="${this.title}" data-price="${this.price}" data-id="${this.id}"><h3>${this.title}</h3><p>${this.price}</p></div>`
-    }
-}
-
-class Cart {
-    constructor() {
-        this.goods = []
+        return `<div class="cart-item" data-title="${this.title}" data-price="${this.price}" id="${this.id}"><h3>${this.title}</h3><p>${this.price}</p><button class="remove-from-cart-button" type="button"><i class="fas fa-shopping-basket"></i></button></div>`
     }
 }
 
 const init = async () => {
-    const list = new GoodsList()
+    const goodsList = new GoodsList()
     const cartList = new Cart()
-    await list.fetchGoods()
-    list.render()
-    list.clickHandlers()
-};
+    await goodsList.fetchGoods()
+    goodsList.render()
+    goodsList.clickHandlers(cartList)
+}
 
 window.onload = init;
 
